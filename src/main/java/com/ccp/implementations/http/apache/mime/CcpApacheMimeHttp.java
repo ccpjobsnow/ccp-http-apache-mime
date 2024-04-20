d0364d5c864f48ab268a85d7d12e6f3ff6e0c9e2
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:29 -0300

    .

--- src/main/java/com/ccp/implementations/http/apache/mime/CcpApacheMimeHttp.java
+++ src/main/java/com/ccp/implementations/http/apache/mime/CcpApacheMimeHttp.java
@@ -1,12 +1,11 @@
 package com.ccp.implementations.http.apache.mime;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.http.CcpHttpRequester;
 
-public class CcpApacheMimeHttp implements CcpInstanceProvider {
+public class CcpApacheMimeHttp implements CcpInstanceProvider<CcpHttpRequester> {
 
-	
-	public Object getInstance() {
+	public CcpHttpRequester getInstance() {
 		return new ApacheMimeHttpRequester();
 	}
-
-}// meu teste 
+}
